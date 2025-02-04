bf587f7ad7f0f1bbbb07c45dbdf87bb402ed212b
estrelarato <b.caio@escolar.ifrn.edu.br>
2025-02-04 16:35:04 -0300

    f

--- batistuta/Assets/script/player.cs
+++ batistuta/Assets/script/player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Player-related variables
     private Rigidbody2D rb;
     private float moveX;
     private Animator anim; 
@@ -13,19 +14,22 @@ public class Player : MonoBehaviour
     public bool noChao;
     public int addJumps;
 
+    // Initialize components
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump"))
         {
-            if (noChao || addJumps > 0)
+            // Check jump conditions
+            if (noChao || addJumps == 0)
             {
                 Jump();
                 if (!noChao) addJumps--;
@@ -33,50 +37,71 @@ public class Player : MonoBehaviour
         }
     }
 
+    // FixedUpdate is used for physics-based movements
     void FixedUpdate()
     {
         Move();
+
+        if (noChao)
+        {
+            addJumps = 1;
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump") && addJumps > 0)
+            {
+                addJumps--;
+                Jump();
+            }
+        }
     }
 
+    // Move the player character
     void Move()
     {
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
 
-        if(moveX > 0)
+        if (moveX > 0)
         {
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f); // Face right
             anim.SetBool("isRun", true);
         }
-
-        if(moveX < 0)
+        else if (moveX < 0)
         {
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f); // Face left
             anim.SetBool("isRun", true);
         }
-        if(moveX == 0)
+        else
         {
-            anim.SetBool("isRun", false);
+            anim.SetBool("isRun", false); // Stop running animation
         }
     }
 
+    // Handle jumping behavior
     void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        anim.SetBool("isJump", true);
+        anim.SetBool("isJump", true); // Trigger jump animation
     }
 
+    // Detect collision with the ground
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "chao")
+        if (collision.gameObject.CompareTag("chao"))
         {
             noChao = true;
-            anim.SetBool("isJump", false); // Quando o jogador encosta no chão, ele pode pular novamente
+            anim.SetBool("isJump", false); // Stop jump animation when grounded
         }
     }
 
+    // Detect when player leaves the ground
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "chao")
+        if (collision.gameObject.CompareTag("chao"))
         {
             noChao = false;
         }
